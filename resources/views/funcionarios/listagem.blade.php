2ae996f39dc21c63919824f0033df97f1282a962
joao <jrosac2002@academico.ufs.br>
2025-08-06 12:27:06 -0300

    feat: criação da página de listagem

---
+++ resources/views/funcionarios/listagem.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Página de listagem de funcionários</title>
+</head>
+<body>
+    <h1>Página de listagem de funcionários</h1>
+
+</body>
+</html>
