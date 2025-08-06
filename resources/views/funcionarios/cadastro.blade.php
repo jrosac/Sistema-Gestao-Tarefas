0a0ddd3ee7fc9233841654d0dfff85f4a8c980cb
joao <jrosac2002@academico.ufs.br>
2025-08-06 12:26:52 -0300

    feat: criação da página de cadastro

---
+++ resources/views/funcionarios/cadastro.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Página de cadastro de clientes</title>
+</head>
+<body>
+
+       <div class="content">
+
+        <h1>Página de cadastro de funcionários</h1>
+
+        <div class="formulario">
+
+            <h1>Formulário de cadastro</h1>
+
+            <form method="POST">
+
+                <label for="nome">Digite seu nome:</label>
+                <input type="text" placeholder="Ex: Fábio dos santos" name="nome" required>
+
+                <label for="data-nascimento">Selecione sua data de nascimento:</label>
+                <input type="date" name="data-nascimento" required>
+
+                <label for="cpf">Digite seu CPF</label>
+                <input type="text" placeholder="Ex: 000.000.000-00" name="cpf" required>
+
+                <label for="nome-cargo">Digite seu cargo</label>
+                <input type="text" placeholder="Ex: Desenvolvedor front-end" name="nome-cargo" required>
+
+                <input type="submit" value="Cadastrar">
+
+                <butto></button>
+
+            </form>
+
+
+        </div>
+
+
+
+
+    </div>
+
+</body>
+</html>
