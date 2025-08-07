6f26c972f92a81fef203d312d3b19fa6de2c52b1
joao <jrosac2002@academico.ufs.br>
2025-08-07 09:02:16 -0300

    feat: adicionei o endereco na parte de cadastro

--- resources/views/funcionarios/cadastro.blade.php
+++ resources/views/funcionarios/cadastro.blade.php
@@ -29,16 +29,38 @@
 
                 <br><br>
 
-                <label for="cpf">Digite seu CPF</label>
+                <label for="cpf">Digite seu CPF:</label>
                 <input type="text" placeholder="Ex: 000.000.000-00" name="cpf" required>
 
                 <br><br>
 
-                <label for="nome-cargo">Digite seu cargo</label>
+                <label for="nome-cargo">Digite seu cargo:</label>
                 <input type="text" placeholder="Ex: Desenvolvedor front-end" name="nome-cargo" required>
+
+                <br>
+
+                <h3>endereço:</h3>
+
+                <label for="logradouro">Logradouro:</label>
+                <input type="text" placeholder="Ex: Rua Major Santos Guedes " name="logradouro" required>
 
                 <br><br>
 
+                <label for="numero">Número:</label>
+                <input type="text" placeholder="Ex: 941" name="numero" required>
+
+                <br><br>
+
+                <label for="cidade">Cidade:</label>
+                <input type="text" placeholder="Ex: Aracaju" required>
+
+                <br><br>
+
+                <label for="estado">Estado:</label>
+                <input type="text" placeholder="Ex: Sergipe" required>
+
+
+                <br><br>
                 <input type="submit" value="Cadastrar">
 
            </form>
