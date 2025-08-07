65b18efe3f148270eaf8424b27791ea2efb799dc
joao <jrosac2002@academico.ufs.br>
2025-08-07 09:25:34 -0300

    feat: adicionado a estrutura inicial da pagina individual

---
+++ resources/views/funcionarios/pagina_individual.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Página do funcionário</title>
+</head>
+<body>
+
+    <div class="content">
+
+    <h1>Página individual do funcionário</h1>
+
+    <br><hr>
+
+    <div class="card">
+
+        <h2>Informações do funcionário</h2>
+
+        <div class="info">
+             <p><h3>Nome:</h3>Fulano da silva</p>
+             <p><h3>CPF:</h3>XXX.XXX.XXX-XX</p>
+             <p><h3>Data de nascimento:</h3>XX/XX/XXXX</p>
+             <p><h3>Cargo:</h3>Desenvolvedor</p>
+             <p><h3>Logradouro:</h3>Alameda das florestas</p>
+             <p><h3>Número:</h3>941</p>
+             <p><h3>Cidade:</h3>Aracaju</p>
+             <p><h3>Estado</h3>Sergipe</p>
+
+             <div class="tasks">
+                <h2>Tarefas atribuidas:</h2>
+                <p>xxxxxxx</p>
+                <p>xxxxxxx</p>
+                <p>xxxxxxx</p>
+            <button>Editar tarefas</button>
+
+             </div>
+
+             <br><br>
+             <div class="btn">
+             <button>Atualizar Dados</button>
+             <button>Deletar funcionário</button>
+
+             </div>
+
+
+
+
+
+       </div>
+
+    </div>
+
+
+
+    </div>
+
+
+</body>
+</html>
