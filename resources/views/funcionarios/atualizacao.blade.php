cf7c136d4cf6ec70af8ee913bc64872ffa02a0a6
joao <jrosac2002@academico.ufs.br>
2025-08-07 09:42:09 -0300

    feat: adicionado a estrutura inicial da pagina de update

---
+++ resources/views/funcionarios/atualizacao.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Página de atualização de dados do funcionario</title>
+</head>
+<body>
+    <div class="content">
+
+    </div>
+    <h1>Atualizacao de dados do funcionario</h1>
+    <br><hr>
+
+                <h1>Formulário de Atualização de Dados</h1>
+
+            <form method="POST">
+                @csrf
+
+                <label for="nome">Digite seu nome:</label>
+                <input type="text" placeholder="Ex: Fábio dos santos" name="nome" required>
+
+                <br><br>
+
+                <label for="data-nascimento">Selecione sua data de nascimento:</label>
+                <input type="date" name="data-nascimento" required>
+
+                <br><br>
+
+                <label for="cpf">Digite seu CPF:</label>
+                <input type="text" placeholder="Ex: 000.000.000-00" name="cpf" required>
+
+                <br><br>
+
+                <label for="nome-cargo">Digite seu cargo:</label>
+                <input type="text" placeholder="Ex: Desenvolvedor front-end" name="nome-cargo" required>
+
+                <br>
+
+                <h3>endereço:</h3>
+
+                <label for="logradouro">Logradouro:</label>
+                <input type="text" placeholder="Ex: Rua Major Santos Guedes " name="logradouro" required>
+
+                <br><br>
+
+                <label for="numero">Número:</label>
+                <input type="text" placeholder="Ex: 941" name="numero" required>
+
+                <br><br>
+
+                <label for="cidade">Cidade:</label>
+                <input type="text" placeholder="Ex: Aracaju" required>
+
+                <br><br>
+
+                <label for="estado">Estado:</label>
+                <input type="text" placeholder="Ex: Sergipe" required>
+
+
+                <br><br>
+                <input type="submit" value="Cadastrar">
+
+           </form>
+
+</body>
+</html>
