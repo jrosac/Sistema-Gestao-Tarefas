5d680e81c4e886d213dfee0f6ed0079ad357f780
joao <jrosac2002@academico.ufs.br>
2025-08-15 10:13:44 -0300

    feat: criação do layout base das paginas, com nav-bar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,27 +1,30 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="{{ str_replace("_","-",app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <title>Página de inicio</title>
+
+    @vite(["resources/css/app.css","resources/js/app.js"])
+
+    <title>Formulário de cadastro</title>
 </head>
 <body>
+
     <div class="main-container">
         <header class="header">
             <div class="content-header">
-                <h2 class="title-logo">Gestão-Tarefas</h2>
-                <ul>
-                    <li><a href="#" class="nav-link">Funcionários</a></li>
+                <h2 class="title-logo"><a href="{{route("home")}}">Gestão-Tarefas</a></h2>
+                <ul class="list-nav-link">
+                    <li><a href="#" class="nav-link">Funcionarios</a></li>
                     <li><a href="#" class="nav-link">Tarefas</a></li>
+
                 </ul>
+
             </div>
+
         </header>
-
-        @yield('content')
-
+      @yield('content')
     </div>
-
 </body>
 </html>
