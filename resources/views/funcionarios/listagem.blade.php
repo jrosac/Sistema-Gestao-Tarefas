fa5a2b08e9fbc7228689a6481ec68c58997c6b41
joao <jrosac2002@academico.ufs.br>
2025-08-18 10:10:24 -0300

    feat: estilização nas páginas de listagem

--- resources/views/funcionarios/listagem.blade.php
+++ resources/views/funcionarios/listagem.blade.php
@@ -2,26 +2,39 @@
 
 @section('content')
 
-    <h1>Página de listagem de funcionários</h1>
+<div class="max-w-6xl mx-auto p-6">
 
-    <br><hr>
-
-    <table>
-      <tr>
-        <th>Nome</th>
-        <th>CPF</th>
-        <th>D. de nascimento</th>
-        <th>Cargo</th>
-     </tr>
-
-     <tr>
-        <td><a href="/pagina_funcionario">Fulano da silva</a></td>
-        <td>XX.XXX.XXX-XX</td>
-        <td>XX/XX/XXXX</td>
-        <td>Desenvolvedor</td>
-     </tr>
+        <h1 class="text-2xl font-semibold text-gray-800 text-center pt-1 pb-8">Listagem de Funcionários</h1>
 
 
-    </table>
 
+    <div class="bg-white shadow-sm rounded-lg overflow-hidden">
+        <div class="overflow-x-auto">
+            <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nome</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">CPF</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">D. de nascimento</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Cargo</th>
+
+                    </tr>
+                </thead>
+
+                <tbody class="bg-white divide-y divide-gray-100">
+                    <tr class="hover:bg-gray-50">
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-indigo-600 font-medium">
+                            <a href="/pagina_funcionario" class="hover:underline text-black ">Fulano da Silva</a>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-700">XX.XXX.XXX-XX</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-700">XX/XX/XXXX</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-700">Desenvolvedor</td>
+
+                    </tr>
+
+
+
+                </tbody>
+            </table>
+        </div>
 @endsection
--- resources/views/tarefas/listagem.blade.php
+++ resources/views/tarefas/listagem.blade.php
@@ -2,26 +2,39 @@
 
 @section('content')
 
-<h1>Página de listagem de tarefas</h1>
+<div class="max-w-6xl mx-auto p-6">
 
-    <br><hr>
-
-    <table>
-      <tr>
-        <th>Nome</th>
-        <th>Detalhes</th>
-        <th>Data de entrega</th>
-        <th>Status</th>
-     </tr>
-
-     <tr>
-        <td><a href={{route("tarefa.pagina_individual")}}>Testar o ambiente de desenvolvimento</a></td>
-        <td>Utilizar o docker para baixar as dependecias e testar a aplicação em um ambiente isolado</td>
-        <td>XX/XX/XXXX</td>
-        <td>A fazer</td>
-     </tr>
+        <h1 class="text-2xl font-semibold text-gray-800 text-center pt-1 pb-8">Listagem de Tarefas</h1>
 
 
-    </table>
 
+    <div class="bg-white shadow-sm rounded-lg overflow-hidden">
+        <div class="overflow-x-auto">
+            <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nome</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Dealhes</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">D. de entrega</th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
+
+                    </tr>
+                </thead>
+
+                <tbody class="bg-white divide-y divide-gray-100">
+                    <tr class="hover:bg-gray-50">
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-indigo-600 font-medium">
+                            <a href="/pagina_funcionario" class="hover:underline text-black">Testar ambiente de Desenvolvimetno</a>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-700 truncate">Utilizar o docker para baixar as dependecias e testar a aplicação em um ambiente isolado</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-700">XX/XX/XXXX</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-700">A fazer</td>
+
+                    </tr>
+
+
+
+                </tbody>
+            </table>
+        </div>
 @endsection
