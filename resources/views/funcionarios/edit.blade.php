b39ab09b75f22cc970f713b035abe244c85af143
joao <jrosac2002@academico.ufs.br>
2025-08-20 12:05:28 -0300

    feat: logica para aparecer dinamicamente os dados ja cadastrados dentro dos inputs

--- resources/views/funcionarios/edit.blade.php
+++ resources/views/funcionarios/edit.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 
+
 <div class="flex justify-center  bg-gray-100 mt-18" >
     <div class="bg-white shadow-xl rounded-2xl p-8 w-full max-w-4xl">
 
@@ -26,25 +27,25 @@
                 <div class="space-y-4">
                     <div>
                         <label for="nome" class="block text-sm font-medium text-gray-700">Digite seu nome:</label>
-                        <input type="text" name="nome" placeholder="Ex: Fábio dos Santos"
+                        <input type="text" name="nome" placeholder="Ex: Fábio dos Santos" value="{{$funcionario->nome}}"
                             class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
                     </div>
 
                     <div>
                         <label for="data-nascimento" class="block text-sm font-medium text-gray-700">Selecione sua data de nascimento:</label>
-                        <input type="date" name="data-nascimento"
+                        <input type="date" name="data-nascimento" value="{{$funcionario->data_nascimento}}"
                             class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
                     </div>
 
                     <div>
                         <label for="cpf" class="block text-sm font-medium text-gray-700">Digite seu CPF:</label>
-                        <input type="text" name="cpf" placeholder="Ex: 000.000.000-00"
+                        <input type="text" name="cpf" placeholder="Ex: 000.000.000-00" value="{{$funcionario->cpf}}"
                             class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
                     </div>
 
                     <div>
                         <label for="nome-cargo" class="block text-sm font-medium text-gray-700">Digite seu cargo:</label>
-                        <input type="text" name="nome-cargo" placeholder="Ex: Desenvolvedor Front-end"
+                        <input type="text" name="nome-cargo" placeholder="Ex: Desenvolvedor Front-end" value="{{$funcionario->cargo}}"
                             class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
                     </div>
                 </div>
@@ -53,25 +54,25 @@
                 <div class="space-y-4">
                     <div>
                         <label for="logradouro" class="block text-sm font-medium text-gray-700">Logradouro:</label>
-                        <input type="text" name="logradouro" placeholder="Ex: Rua Major Santos Guedes"
+                        <input type="text" name="logradouro" placeholder="Ex: Rua Major Santos Guedes" value="{{$funcionario->endereco->logradouro}}"
                             class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
                     </div>
 
                     <div>
                         <label for="numero" class="block text-sm font-medium text-gray-700">Número:</label>
-                        <input type="text" name="numero" placeholder="Ex: 941"
+                        <input type="text" name="numero" placeholder="Ex: 941" value="{{$funcionario->endereco->numero}}"
                             class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
                     </div>
 
                     <div>
                         <label for="cidade" class="block text-sm font-medium text-gray-700">Cidade:</label>
-                        <input type="text" name="cidade" placeholder="Ex: Aracaju"
+                        <input type="text" name="cidade" placeholder="Ex: Aracaju" value="{{$funcionario->endereco->cidade}}"
                             class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
                     </div>
 
                     <div>
                         <label for="estado" class="block text-sm font-medium text-gray-700">Estado:</label>
-                        <input type="text" name="estado" placeholder="Ex: Sergipe"
+                        <input type="text" name="estado" placeholder="Ex: Sergipe" value="{{$funcionario->endereco->estado}}"
                             class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
                     </div>
                 </div>
--- resources/views/tarefas/edit.blade.php
+++ resources/views/tarefas/edit.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 
+
 <div class="flex justify-center  bg-gray-100 mt-18" >
     <div class="bg-white shadow-xl rounded-2xl p-8 w-full max-w-4xl">
 
@@ -26,20 +27,54 @@
                 <div class="space-y-4">
                     <div>
                         <label for="nome" class="block text-sm font-medium text-gray-700">Digite o nome da tarefa</label>
-                        <input type="text" name="nome" placeholder="Ex: Fábio dos Santos"
+                        <input type="text" name="nome" placeholder="Ex: Fábio dos Santos" value="{{$tarefa->titulo}}"
                             class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
                     </div>
 
                     <div>
                         <label for="detalhe" class="block text-sm font-medium text-gray-700">Digite os detalhes da tarefa:</label>
                         <input type="text"  placeholder="Ex: faça um crud utilzando tal tenologia e teste em tal ambiente" name="detalhe"
+                            value="{{$tarefa->descricao}}"
                             class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
                     </div>
+
+                       <div>
+                       <label class="block text-sm font-medium text-gray-700 mb-2">
+                           Selecione um status para a tarefa:
+                       </label>
+
+                       <div class="flex flex-col gap-2">
+                           <label class="inline-flex items-center">
+                               <input type="radio" name="status_id" value="1"
+                                   class="text-blue-600 focus:ring-blue-500 border-gray-300"
+                                   {{ old('status_id') == 1 ? 'checked' : '' }}>
+                               <span class="ml-2 text-sm">A fazer</span>
+                           </label>
+
+                           <label class="inline-flex items-center">
+                               <input type="radio" name="status_id" value="2"
+                                   class="text-blue-600 focus:ring-blue-500 border-gray-300"
+                                   {{ old('status_id') == 2 ? 'checked' : '' }}>
+                               <span class="ml-2 text-sm">Fazendo</span>
+                           </label>
+
+                           <label class="inline-flex items-center">
+                               <input type="radio" name="status_id" value="3"
+                                   class="text-blue-600 focus:ring-blue-500 border-gray-300"
+                                   {{ old('status_id') == 3 ? 'checked' : '' }}>
+                               <span class="ml-2 text-sm">Feito</span>
+                           </label>
+
+                           @error('status_id')
+                               <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
+                           @enderror
+                       </div>
+                   </div>
 
 
                     <div>
                         <label for="data-entrega" class="block text-sm font-medium text-gray-700">Digite a data de entrega:</label>
-                        <input type="date" name="data-entrega"
+                        <input type="date" name="data-entrega" value="{{$tarefa->data_entrega}}"
                             class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
                     </div>
                 </div>
@@ -48,7 +83,7 @@
             <div class="flex justify-center">
                 <button type="submit"
                     class="bg-blue-600 text-white px-6 py-2 rounded-lg shadow-md hover:bg-blue-700 transition cursor-pointer">
-                    <a href="{{route("tarefa.listagem")}}">Atualizar</a>
+                    <a href="{{route("tarefa.index")}}">Atualizar</a>
                 </button>
             </div>
         </form>
