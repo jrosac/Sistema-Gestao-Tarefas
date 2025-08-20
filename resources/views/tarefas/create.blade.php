ff1bb67b1babb45ace7012a33f3856c065138262
joao <jrosac2002@academico.ufs.br>
2025-08-20 09:41:01 -0300

    feat: adicionado o campo status no formulario

--- resources/views/tarefas/create.blade.php
+++ resources/views/tarefas/create.blade.php
@@ -19,13 +19,10 @@
                 <h2 class="text-xl font-semibold text-gray-700">Formulário de Cadastro</h2>
             </div>
 
-            <!-- Campos em duas colunas -->
 
-
-                <!-- Coluna Esquerda -->
                 <div class="space-y-4">
                     <div>
-                        <label for="nome" class="block text-sm font-medium text-gray-700">Digite o nome da tarefa</label>
+                        <label for="no" class="block text-sm font-medium text-gray-700">Digite o nome da tarefa</label>
                         <input type="text" name="nome" placeholder="Ex: Fábio dos Santos"
                             class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
                     </div>
@@ -35,6 +32,32 @@
                         <input type="text"  placeholder="Ex: faça um crud utilzando tal tenologia e teste em tal ambiente" name="detalhe"
                             class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
                     </div>
+
+                   <div>
+                     <label class="block text-sm font-medium text-gray-700 mb-2">
+                       Selecione um status para a tarefa:
+                     </label>
+
+                     <div class="flex flex-col gap-2">
+                       <label class="inline-flex items-center">
+                         <input type="radio" name="status" value="a fazer"
+                           class="text-blue-600 focus:ring-blue-500 border-gray-300">
+                         <span class="ml-2 text-sm">A fazer</span>
+                       </label>
+
+                       <label class="inline-flex items-center">
+                         <input type="radio" name="status" value="fazendo"
+                           class="text-blue-600 focus:ring-blue-500 border-gray-300">
+                         <span class="ml-2 text-sm">Fazendo</span>
+                       </label>
+
+                       <label class="inline-flex items-center">
+                         <input type="radio" name="status" value="feito"
+                           class="text-blue-600 focus:ring-blue-500 border-gray-300">
+                         <span class="ml-2 text-sm">Feito</span>
+                       </label>
+                     </div>
+                   </div>
 
 
                     <div>
