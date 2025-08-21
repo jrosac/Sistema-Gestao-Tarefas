47fbf16ad3f3f1c8a9dcfa7baa8e5d82adb4f642
joao <jrosac2002@academico.ufs.br>
2025-08-21 08:41:43 -0300

    feat: adicionei o método store

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -35,7 +35,35 @@ class FuncionarioController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            //fucionario
+            'nome'=> 'required|string|max:255',
+            'cpf'=> 'required|string|max:14',
+            'cargo'=> 'required|string|max:100',
+            'data_nascimento'=> 'required|date',
+            // endereco
+            'logradouro'=> 'required|string|max:255',
+            'numero'=> 'required|string|max:10',
+            'cidade'=> 'required|string|max:100',
+            'estado'=> 'required|string|max:25',
+
+        ]);
+
+        $funcionario = Funcionario::create([
+            'nome'=> $validated['nome'],
+            'cpf'=> $validated['cpf'],
+            'cargo'=> $validated['cargo'],
+            'data_nascimento'=> $validated['data_nascimento'],
+        ]);
+
+        $funcionario->endereco()->create([
+            'logradouro'=> $validated['logradouro'],
+            'numero'=> $validated['numero'],
+            'cidade'=> $validated['cidade'],
+            'estado'=> $validated['estado'],
+        ]);
+
+        return redirect()->route('funcionario.index')->with('success', 'Funcionário cadastrado com sucesso!');
     }
 
     /**
