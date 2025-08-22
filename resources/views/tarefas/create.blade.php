839f9d78831b0be929824c8cb569feb20000ca9c
joao <jrosac2002@academico.ufs.br>
2025-08-22 09:01:01 -0300

    feat: adicionei os campos selecionar os funcionários para as tarefas

--- resources/views/tarefas/create.blade.php
+++ resources/views/tarefas/create.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+
+
 <div class="flex justify-center  bg-gray-100 mt-18" >
     <div class="bg-white shadow-xl rounded-2xl p-8 w-full max-w-4xl">
 
@@ -13,19 +15,6 @@
         <!-- Formulário -->
 <form action="{{ route('tarefa.store') }}" method="POST" class="space-y-6">
     @csrf
-
-    <!-- Mensagens de erro -->
-    @if($errors->any())
-        <div class="bg-red-100 text-red-800 p-3 mb-4 rounded">
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <!-- Subtítulo -->
     <div class="text-center mb-6">
         <h2 class="text-xl font-semibold text-gray-700">Formulário de Cadastro</h2>
     </div>
@@ -37,9 +26,6 @@
             <label for="titulo" class="block text-sm font-medium text-gray-700">Digite o título da tarefa</label>
             <input type="text" name="titulo" value="{{ old('titulo') }}" placeholder="Ex: Fazer upload do banco de dados"
                 class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
-            @error('titulo')
-                <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
-            @enderror
         </div>
 
         <!-- Descrição -->
@@ -47,9 +33,6 @@
             <label for="descricao" class="block text-sm font-medium text-gray-700">Digite os detalhes da tarefa:</label>
             <input type="text" name="descricao" value="{{ old('descricao') }}" placeholder="Ex: faça um CRUD utilizando tal tecnologia"
                 class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
-            @error('descricao')
-                <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
-            @enderror
         </div>
 
         <!-- Status -->
@@ -79,21 +62,43 @@
                         {{ old('status_id') == 3 ? 'checked' : '' }}>
                     <span class="ml-2 text-sm">Feito</span>
                 </label>
-
-                @error('status_id')
-                    <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
-                @enderror
             </div>
         </div>
+
+<div>
+    <label for="funcionarios" class="block text-sm font-medium text-gray-700 mb-1">Selecione os funcionários:</label>
+
+    <select name="funcionarios[]" id="funcionario1"
+        class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-2 bg-white text-gray-700 mb-3">
+        <option value="" disabled selected >Escolha um funcionário...</option>
+        @foreach ($funcionarios as $funcionario)
+            <option value="{{ $funcionario->id }}">{{ $funcionario->nome }}</option>
+        @endforeach
+    </select>
+
+        <select name="funcionarios[]" id="funcionario2"
+        class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-2 bg-white text-gray-700 mb-3">
+        <option value="" disabled selected >Escolha um funcionário...</option>
+        @foreach ($funcionarios as $funcionario)
+            <option value="{{ $funcionario->id }}">{{ $funcionario->nome }}</option>
+        @endforeach
+    </select>
+
+        <select name="funcionarios[]" id="funcionario3"
+        class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-2 bg-white text-gray-700 mb-3">
+        <option value="" disabled selected >Escolha um funcionário...</option>
+        @foreach ($funcionarios as $funcionario)
+            <option value="{{ $funcionario->id }}">{{ $funcionario->nome }}</option>
+        @endforeach
+    </select>
+</div>
+
 
         <!-- Data de Entrega -->
         <div>
             <label for="data_entrega" class="block text-sm font-medium text-gray-700">Digite a data de entrega:</label>
             <input type="date" name="data_entrega" value="{{ old('data_entrega') }}"
                 class="mt-1 w-full rounded-lg border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 p-1">
-            @error('data_entrega')
-                <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
-            @enderror
         </div>
 
     </div>
@@ -110,5 +115,4 @@
 
     </div>
 </div>
-
 @endsection
