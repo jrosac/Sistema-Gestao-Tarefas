8edf815938902c81407cfe1a95f76b34ccdad114
joao <jrosac2002@academico.ufs.br>
2025-08-27 09:23:03 -0300

    feat: utilizei resource para instanciar todas as rotas de tarefas e funcionarios

--- routes/web.php
+++ routes/web.php
@@ -10,34 +10,38 @@ Route::get('/', function () {
 
 //--------------------------------------------------------
 
-Route::get('/cadastro-funcionario',[FuncionarioController::class,'create'])->name("funcionario.create");
+// Route::get('/cadastro-funcionario',[FuncionarioController::class,'create'])->name("funcionario.create");
 
-Route::post('/cadastro-funcionario',[FuncionarioController::class,'store'])->name('funcionario.store');
+// Route::post('/cadastro-funcionario',[FuncionarioController::class,'store'])->name('funcionario.store');
 
-Route::get('/index-funcionario',[FuncionarioController::class,'index'])->name('funcionario.index');
+// Route::get('/index-funcionario',[FuncionarioController::class,'index'])->name('funcionario.index');
 
-Route::get('funcionario/{id}',[FuncionarioController::class,'show'])->name('funcionario.show');
+// Route::get('funcionario/{id}',[FuncionarioController::class,'show'])->name('funcionario.show');
 
-Route::get('funcionario/{id}/edit',[FuncionarioController::class,'edit'])->name('funcionario.edit');
+// Route::get('funcionario/{id}/edit',[FuncionarioController::class,'edit'])->name('funcionario.edit');
 
-Route::put('funcionario/{id}',[FuncionarioController::class,'update'])->name('funcionario.update');
+// Route::put('funcionario/{id}',[FuncionarioController::class,'update'])->name('funcionario.update');
 
-Route::delete('funcionario/{id}',[FuncionarioController::class,'destroy'])->name('funcionario.destroy');
+// Route::delete('funcionario/{id}',[FuncionarioController::class,'destroy'])->name('funcionario.destroy');
+
+Route::resource('funcionario', FuncionarioController::class);
 
 // -----------------------------------------------------------
 
 
 
-Route::get('/cadastro-tarefa',[ TarefaController::class,'create'])->name("tarefa.create");
+// Route::get('/cadastro-tarefa',[ TarefaController::class,'create'])->name("tarefa.create");
 
-Route::get('/index-tarefa',[TarefaController::class,'index'] )->name("tarefa.index");
+// Route::get('/index-tarefa',[TarefaController::class,'index'] )->name("tarefa.index");
 
-Route::post('/cadastro-tarefa', [TarefaController::class, 'store'])->name('tarefa.store');
+// Route::post('/cadastro-tarefa', [TarefaController::class, 'store'])->name('tarefa.store');
 
-Route::get('/tarefa/{id}',[TarefaController::class,'show'])->name('tarefa.show');
+// Route::get('/tarefa/{id}',[TarefaController::class,'show'])->name('tarefa.show');
 
-Route::get('tarefa/{id}/edit',[TarefaController::class,'edit'])->name('tarefa.edit');
+// Route::get('tarefa/{id}/edit',[TarefaController::class,'edit'])->name('tarefa.edit');
 
-Route::put('tarefa/{id}',[TarefaController::class,'update'])->name('tarefa.update');
+// Route::put('tarefa/{id}',[TarefaController::class,'update'])->name('tarefa.update');
 
-Route::delete('/tarefa/{id}',[TarefaController::class,'destroy'])->name('tarefa.destroy');
+// Route::delete('/tarefa/{id}',[TarefaController::class,'destroy'])->name('tarefa.destroy');
+
+Route::resource('tarefa', TarefaController::class);
