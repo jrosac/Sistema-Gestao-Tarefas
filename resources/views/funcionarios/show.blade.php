218e6a2f4ef101097e3e13ec255c0b19abb471e7
joao <jrosac2002@academico.ufs.br>
2025-08-28 08:54:15 -0300

    feat: adicionei um componente condicional para funcionários sem tarefas atribuidas

--- resources/views/funcionarios/show.blade.php
+++ resources/views/funcionarios/show.blade.php
@@ -65,7 +65,14 @@
 <div class="bg-white rounded-lg p-5 border border-gray-100 shadow-sm">
     <h2 class="text-lg font-semibold text-gray-800 mb-4">Tarefas atribuídas</h2>
 
-    <div class="overflow-x-auto">
+    @if($funcionario->tarefas->isEmpty())
+        <div>
+            <div class="text-xl pb-3">Este funcionário ainda não possui tarefas atribuídas.</div>
+            <p>Você pode <a href="{{route("tarefa.index")}}" class="text-blue-500 hover:text-blue-800 transition-colors duration-500">visualizar as tarefas existentes</a> ou
+                <a href="{{route("tarefa.create")}}" class="text-blue-500 hover:text-blue-800 transition-colors duration-500">cadastrar uma nova tarefa</a> </p>
+        </div>
+    @else
+            <div class="overflow-x-auto">
         <table class="min-w-full border border-gray-200 divide-y divide-gray-200 text-sm">
             <thead class="bg-gray-100">
                 <tr>
@@ -95,6 +102,7 @@
         </table>
     </div>
 </div>
+@endif
 
 
     </div>
